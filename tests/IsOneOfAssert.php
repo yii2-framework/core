ce64804264bb2c51ff2d1e752e219cf1fc694784
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-03-14 19:19:51 -0300

    chore: adjust code style. (#52)

--- tests/IsOneOfAssert.php
+++ tests/IsOneOfAssert.php
@@ -1,12 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license https://www.yiiframework.com/license/
  */
-
-declare(strict_types=1);
 
 namespace yiiunit;
 
@@ -36,7 +36,7 @@ class IsOneOfAssert extends Constraint
         $allowedValues = [];
 
         foreach ($this->allowedValues as $value) {
-            $this->allowedValues[] = VarDumper::dumpAsString($value);
+            $allowedValues[] = VarDumper::dumpAsString($value);
         }
 
         $expectedAsString = implode(', ', $allowedValues);
